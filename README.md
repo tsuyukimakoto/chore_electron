e7eb5b3f6679ae1aaa489487bf9be3dd5ad8fb73
makoto tsuyuki <mtsuyuki@gmail.com>
2022-07-20 21:49:39 +0900

    just run and build

--- README.md
+++ README.md
@@ -1,1 +1,30 @@
 # chore_electron
+
+起動
+
+```
+$ npx electron ./src
+```
+
+パッケージング
+
+```
+$ npm install -D electron-packager
+
+$ npx electron-packager src MyApp --platform=darwin --arch=x64 --overwrite
+
+$ npx electron-packager src MyApp --platform=darwin --arch=arm64 --overwrite
+
+$ npx electron-packager src MyApp --platform=darwin --arch=universal --overwrite
+```
+
+```
+$ npm install -D electron-builder
+
+$ npx electron-builder --dir --mac --universal
+```
+
+## typescript
+
+$ yarn create electron-app MyTypescriptApp --template=typescript-webpack
+
---
+++ src/main.js
@@ -0,0 +1,40 @@
+const { app, BrowserWindow } = require("electron");
+const path = require("path");
+
+let mainWindow;
+
+const createWindow = () => {
+    mainWindow = new BrowserWindow({
+        width: 800,
+        height: 600,
+        webPreferences: {
+            preload: path.join(__dirname, "preload.js"),
+        },
+    });
+
+    mainWindow.loadFile("index.html");
+    mainWindow.webContents.openDevTools();
+
+    mainWindow.on("closed", () => {
+        mainWindow = null;
+    });
+};
+
+app.whenReady().then(() => {
+    createWindow();
+
+    app.on("activate", () => {
+        if (BrowserWindow.getAllWindows().length === 0) {
+            createWindow();
+        }
+    });
+});
+
+app.on("window-all-closed", () => {
+    if (process.platform !== "darwin") {
+        app.quit();
+    }
+});
+
+
+
